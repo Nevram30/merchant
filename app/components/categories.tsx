d27f827a34b8bf7279c1bf52962cd93f21d16d11
nevram30 <68675200+Nevram30@users.noreply.github.com>
2023-09-30 20:20:05 +0800

    feat: add categories

--- app/components/categories.tsx
+++ app/components/categories.tsx
@@ -1,16 +1,83 @@
 import React from "react";
+import Image from "next/image";
 
 type Props = {};
 
 const categories = (props: Props) => {
   return (
-    <div className="box justify-center text-center space-x-2 my-24">
-      <button className="btn btn-active">Default</button>
-      <button className="btn btn-active btn-neutral">Neutral</button>
-      <button className="btn btn-active btn-primary">Primary</button>
-      <button className="btn btn-active btn-secondary">Secondary</button>
-      <button className="btn btn-active btn-accent">Accent</button>
-      <button className="btn btn-active btn-ghost">Ghost</button>
+    <div className="box justify-center text-center space-x-9 my-24">
+      <button className="btn btn-active h-20">
+        <div className="m-0">
+          <div className="px-8">
+            <Image
+              alt="shoes"
+              src="/united-states.png"
+              className="object-cover"
+              width={20}
+              height={20}
+            />
+          </div>
+          <p className="pt-2">Coins</p>
+        </div>
+      </button>
+      <button className="btn btn-active h-20">
+        <div className="m-0">
+          <div className="px-10">
+            <Image
+              alt="shoes"
+              src="/united-states.png"
+              className="object-cover"
+              width={20}
+              height={20}
+            />
+          </div>
+          <p className="pt-2">Merchant Mall</p>
+        </div>
+      </button>
+      <button className="btn btn-active h-20">
+        <div className="m-0">
+          <div className="box px-12">
+            <Image
+              alt="shoes"
+              src="/united-states.png"
+              className="object-cover"
+              width={20}
+              height={20}
+            />
+          </div>
+          <p className="pt-2">Claim Vouchers</p>
+        </div>
+      </button>
+      <button className="btn btn-active h-20">
+        <div className="m-0">
+          <div className="px-8">
+            <Image
+              alt="shoes"
+              src="/united-states.png"
+              className="object-cover"
+              width={20}
+              height={20}
+            />
+          </div>
+          <p className="pt-2">Promos</p>
+        </div>
+      </button>
+      <button className="btn btn-active h-20">
+        <div className="m-0">
+          <div className="px-8">
+            <Image
+              alt="shoes"
+              src="/united-states.png"
+              className="object-cover"
+              width={20}
+              height={20}
+            />
+          </div>
+          <p className="pt-2">Fresh Sales</p>
+        </div>
+      </button>
     </div>
   );
 };
+
+export default categories;
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Carousel from "@/app/components/carousel";
 import Subnavbar from "@/app/components/subnavbar";
 import Footer from "@/app/components/footer";
 import Multiplebuttons from "@/app/components/multiplebuttons";
+import Categories from "@/app/components/categories";
 
 export default function Home() {
   return (
@@ -14,6 +15,9 @@ export default function Home() {
         <div className="">
           <Carousel />
         </div>
+      </div>
+      <div>
+        <Categories />
       </div>
       <section className="products_container px-5">
         <div className="products_card space-x-3">
